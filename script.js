281b6f5e63c232fd1f048033687c8204f297150c
Jhonnathan-Jhonny <jhonnathan.rodrigues@aluno.uepb.edu.br>
2023-08-05 18:50:15 -0300

    funções básicas da calculadora funcionando corretamente

--- script.js
+++ script.js
@@ -1,35 +1,48 @@
-let cal = document.getElementById('igual')
-let botoesNumericos = document.getElementsByClassName('numero')
-let botoesOperacoes = document.getElementsByClassName('operacoes')
-let botaoLimpar = document.getElementById('limpar')
-let resultado = document.getElementById('res')
+let resultado = document.getElementById('res');
+let botoesNumericos = document.getElementsByClassName('numero');
+let botoesOperacoes = document.getElementsByClassName('operacoes');
+let botaoLimpar = document.getElementById('limpar');
 
-let numero = ''
+let numero = '';
 
 // Adicionando ouvinte de evento a todos os botões
 document.querySelectorAll('button').forEach(botao => {
-    botao.addEventListener('click', function() {
-      const valorBotao = this.textContent;
-      if (valorBotao === "=") {
-        calcularResultado();
-      } else if (valorBotao === "CE") {
-        limparCalculadora();
-      } else if (this.classList.contains('numero') || this.classList.contains('operacoes')) {
-        adicionarCaractere(valorBotao);
-      }
-    });
+  botao.addEventListener('click', function() {
+    const valorBotao = this.textContent;
+    if (valorBotao === "=") {
+      calcularResultado();
+    } else if (valorBotao === "CE") {
+      limparCalculadora();
+    } else if (this.classList.contains('numero') || this.classList.contains('operacoes')) {
+      adicionarCaractere(valorBotao);
+    }
+    else if(valorBotao == 'AC'){
+      apagarUltimoCaracter();
+    }
   });
+});
 
-function adicionarCaractere(caractere){ 
-    numero += caractere
+function adicionarCaractere(caractere) {
+  ultimo = numero;
+  numero += caractere;
+  resultado.value = numero;
+}
+
+function limparCalculadora() {
+  numero = '';
+  resultado.value = '';
+}
+
+function apagarUltimoCaracter(){
+  numero = numero.slice(0,-1);
+  resultado.value = numero
+}
+
+function calcularResultado() {
+  try {
+    numero = math.evaluate(numero);
     resultado.value = numero;
-}
-
-function limparCalculadora(){
-    numero = ''
-    resultado.value = ''
-}
-
-function calcularResultado(){
-
+  } catch (error) {
+    resultado.value = 'Erro';
+  }
 }
